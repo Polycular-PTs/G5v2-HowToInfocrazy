a88d7db3b4fe0e96d4b04efd6d10f6f66215e07f
JaciZigi <jakob.ziegler08@htl-salzburg.ac.at>
2025-12-10 16:18:01 +0100

    Jaci Zigis teil
    Tutorial Code

---
+++ Assets/Scripts/ScriptOnButton.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+public class ScriptOnButton : MonoBehaviour
+{
+    [SerializeField] private TutorialManager tutorialManager;
+    [SerializeField] private TextMeshProUGUI textInButton;
+    [SerializeField] private TextMeshProUGUI correspondingTextMeshProUGUI;
+
+    [Header("If you want to follow an other objects movement")]
+    [SerializeField] private bool isButtonAttachedToOtherObject = false;
+    [SerializeField] private GameObject attachmentObject;
+    [SerializeField] private Vector3 attachmentOffset;
+
+    [Header("If you have a very long text the text takes longer to auto disable")]
+    [SerializeField] private float extraDelayBeforeDisable;
+
+    public bool alreadyEnabled;
+
+    private void Start()
+    {
+        correspondingTextMeshProUGUI.enabled = false;
+        alreadyEnabled = false;
+    }
+
+    public void GiveInfoToTutorialManager()
+    {
+            tutorialManager.ShowInfo(correspondingTextMeshProUGUI, textInButton, extraDelayBeforeDisable, alreadyEnabled);      
+    }
+
+    private void Update()
+    {
+        if (isButtonAttachedToOtherObject)
+        {
+            AttachButtonToObject();
+        }
+    }
+    private void AttachButtonToObject()
+    {
+        GetComponent<Transform>().position = attachmentObject.GetComponentInParent<Transform>().position + attachmentOffset;
+    }
+}
