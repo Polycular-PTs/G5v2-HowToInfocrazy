a2c8aa1ef09447eece5e7d11fe014e5c2357edc8
itsTooooooooom <tomaichhorn@yahoo.com>
2026-01-21 22:32:07 +0100

    DRY: unify scene loading helper (#12); Office Script aufgeräumt, ResetPlayerprefsvalue eingeführt,...

--- Assets/Scripts/OfficeScript.cs
+++ Assets/Scripts/OfficeScript.cs
@@ -7,10 +7,14 @@ using NUnit.Framework.Interfaces;
 public class OfficeScript : MonoBehaviour
 {
     public int score = 100;
-    //public Slider happinessSlider;
     public GameObject happinessFill;
     public TextMeshProUGUI happinessText;
-    private bool firstAnswer;
+
+    string scorePlayerPrefName = "currentScore";
+    string budgetPlayerPrefName = "currentStatebudget";
+    string oppositionPlayerPrefName = "currentOpposition";
+    string functionalityPlayerPrefName = "currentFunctionality";
+    //private bool firstAnswer; //später für Antwortauswertung
 
     public GameObject statebudgetFill;
     public TextMeshProUGUI statebudgetText;
@@ -20,6 +24,14 @@ public class OfficeScript : MonoBehaviour
 
     public GameObject functionalityFill;
     public TextMeshProUGUI functionalityText;
+
+    private void LoadDefeatScene(int whenThisNumberIsZero)
+    {
+        if (whenThisNumberIsZero <= 0)
+        {
+            SceneManager.LoadScene("Defeat");
+        }
+    }
 
     private void ChangeGraphic(int curValue, Transform fillTransform)
     {
@@ -31,145 +43,74 @@ public class OfficeScript : MonoBehaviour
         }
     }
 
+    private void ResetPlayerPrefsValue(string name, Transform fillTransform)
+    {
+        PlayerPrefs.SetInt(name, 100);
+        ChangeGraphic(100, fillTransform.GetComponent<Transform>());
+    }
+
     private void Start()
     {
-        if (!PlayerPrefs.HasKey("currentScore"))
+        if (!PlayerPrefs.HasKey(scorePlayerPrefName))
         {
-            PlayerPrefs.SetInt("currentScore", 100);
-            //Transform fillTransform = happinessFill.GetComponent<Transform>();
-            //Vector3 scale = fillTransform.localScale;
-            //scale.x = 1;
-            //fillTransform.localScale = scale;
-            ChangeGraphic(100, happinessFill.GetComponent<Transform>());
-            //happinessSlider.maxValue = 1;
-            PlayerPrefs.SetString("firstAnswerBoolean", "true");
+            ResetPlayerPrefsValue(scorePlayerPrefName, happinessFill.GetComponent<Transform>());
+            //PlayerPrefs.SetInt("currentScore", 100);
+            //ChangeGraphic(100, happinessFill.GetComponent<Transform>());
+            //PlayerPrefs.SetString("firstAnswerBoolean", "true"); //später für Antwortauswertung
         }
         else
         {
-            int curScore = PlayerPrefs.GetInt("currentScore");
-            Debug.Log("CurrentScore1:" + curScore);
-            //happinessSlider.value = curScore / 100f;
-
+            int curScore = PlayerPrefs.GetInt(scorePlayerPrefName);
             ChangeGraphic(curScore, happinessFill.GetComponent<Transform>());
             happinessText.text = curScore.ToString();
-            if (curScore <= 0) { SceneManager.LoadScene("Defeat"); }
-            //if (curScore < 100f)
-            //{
-            //    Transform fillTransform = happinessFill.GetComponent<Transform>();
-            //    Vector3 scale = fillTransform.localScale;
-            //    scale.x = curScore / 100f;
-            //    fillTransform.localScale = scale;
-            //}
-
-            //if (curScore / 100 > happinessSlider.maxValue)
-            //{
-            //    happinessSlider.maxValue = curScore / 100;
-            //}
-            //if (curScore <= 0)
-            //{
-            //    SceneManager.LoadScene("Defeat");
-            //    //Colour
-            //    //Text mit Defeat oder Win
-            //}
+            LoadDefeatScene(curScore);
+            //if (curScore <= 0) { SceneManager.LoadScene("Defeat"); }
         }
 
 
-        if (!PlayerPrefs.HasKey("currentStatebudget"))
+        if (!PlayerPrefs.HasKey(budgetPlayerPrefName))
         {
-            PlayerPrefs.SetInt("currentStatebudget", 100);
-            //Transform fillTransform = statebudgetFill.GetComponent<Transform>();
-            //Vector3 scale = fillTransform.localScale;
-            //scale.x = 1;
-            //fillTransform.localScale = scale;
-            ChangeGraphic(100, statebudgetFill.GetComponent<Transform>());
+            ResetPlayerPrefsValue(budgetPlayerPrefName, statebudgetFill.GetComponent<Transform>());
+            //PlayerPrefs.SetInt("currentStatebudget", 100);
+            //ChangeGraphic(100, statebudgetFill.GetComponent<Transform>());
         }
         else
         {
             int curState = PlayerPrefs.GetInt("currentStatebudget");
-            Debug.Log("currentStatebudget:" + curState);
-            //happinessSlider.value = curScore / 100f;
-
-            //if (curState < 100f)
-            //{
-            //    Transform fillTransform = statebudgetFill.GetComponent<Transform>();
-            //    Vector3 scale = fillTransform.localScale;
-            //    scale.x = curState / 100f;
-            //    fillTransform.localScale = scale;
-            //}
             ChangeGraphic(curState, statebudgetFill.GetComponent<Transform>());
             statebudgetText.text = curState.ToString();
-            if (curState <= 0) { SceneManager.LoadScene("Defeat"); }
-            //if (curState / 100 > happinessSlider.maxValue)
-            //{
-            //    happinessSlider.maxValue = curState / 100;
-            //}
-            //if (curState <= 0)
-            //{
-            //    //Debug.Log("CurState ist Null");
-            //    SceneManager.LoadScene("Defeat");
-            //    //Colour
-            //    //Text mit Defeat oder Win
-            //}
+            LoadDefeatScene(curState);
+            //if (curState <= 0) { LoadDefeatScene(); }
         }
 
         if (!PlayerPrefs.HasKey("currentOpposition"))
         {
-            PlayerPrefs.SetInt("currentOpposition", 100);
-            ChangeGraphic(100, oppositionFill.GetComponent<Transform>());
-            //Transform fillTransform = oppositionFill.GetComponent<Transform>();
-            //Vector3 scale = fillTransform.localScale;
-            //scale.x = 1;
-            //fillTransform.localScale = scale;
+            ResetPlayerPrefsValue(oppositionPlayerPrefName, oppositionFill.GetComponent<Transform>());
+            //PlayerPrefs.SetInt("currentOpposition", 100);
+            //ChangeGraphic(100, oppositionFill.GetComponent<Transform>());
         }
         else
         {
             int curOppo = PlayerPrefs.GetInt("currentOpposition");
-            //Debug.Log("currentOpposition:" + curState);
             ChangeGraphic(curOppo, oppositionFill.GetComponent<Transform>());
             oppositionText.text = curOppo.ToString();
-            if (curOppo <= 0) { SceneManager.LoadScene("Defeat"); }
-            //if (curOppo < 100f)
-            //{
-            //    Transform fillTransform = oppositionFill.GetComponent<Transform>();
-            //    Vector3 scale = fillTransform.localScale;
-            //    scale.x = curOppo / 100f;
-            //    fillTransform.localScale = scale;
-            //}
-            //if (curOppo <= 0)
-            //{
-            //    Debug.Log("CurState ist Null");
-            //    SceneManager.LoadScene("Defeat");
-            //}
+            LoadDefeatScene(curOppo);
+            //if (curOppo <= 0) { SceneManager.LoadScene("Defeat"); }
         }
 
         if (!PlayerPrefs.HasKey("currentFunctionality"))
         {
-            PlayerPrefs.SetInt("currentFunctionality", 100);
-            ChangeGraphic(100, functionalityFill.GetComponent<Transform>());
-            //Transform fillTransform = functionalityFill.GetComponent<Transform>();
-            //Vector3 scale = fillTransform.localScale;
-            //scale.x = 1;
-            //fillTransform.localScale = scale;
+            ResetPlayerPrefsValue(functionalityPlayerPrefName, functionalityFill.GetComponent<Transform>());
+            //PlayerPrefs.SetInt("currentFunctionality", 100);
+            //ChangeGraphic(100, functionalityFill.GetComponent<Transform>());
         }
         else
         {
             int curFunc = PlayerPrefs.GetInt("currentFunctionality");
-            //Debug.Log("currentFunctionality:" + curFunc);
             ChangeGraphic(curFunc, functionalityFill.GetComponent<Transform>());
             functionalityText.text = curFunc.ToString();
-            if (curFunc <= 0) { SceneManager.LoadScene("Defeat"); }
-            //if (curFunc < 100f)
-            //{
-            //    Transform fillTransform = functionalityFill.GetComponent<Transform>();
-            //    Vector3 scale = fillTransform.localScale;
-            //    scale.x = curFunc / 100f;
-            //    fillTransform.localScale = scale;
-            //}
-            //if (curFunc <= 0)
-            //{
-            //    Debug.Log("CurState ist Null");
-            //    SceneManager.LoadScene("Defeat");
-            //}
+            LoadDefeatScene(curFunc);
+            //if (curFunc <= 0) { SceneManager.LoadScene("Defeat"); }
         }
     }
 
